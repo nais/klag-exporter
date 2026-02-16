8d2b5c521ae999e42ef8990946ee5a97b4cc57e3
@x10an14-nav <christian.chavez@nav.no>
2026-02-16 13:18:29 +0100

    feat(helm): permit configuration of `exporter.performance` settings
    Maybe missed as part of https://github.com/softwaremill/klag-exporter/pull/41 ?

---
+++ flake.nix
@@ -0,0 +1,93 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs =
+    { self, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs { localSystem = { inherit system; }; };
+        inherit (pkgs) lib;
+
+        craneLib = inputs.crane.mkLib pkgs;
+        src = craneLib.cleanCargoSource ./.;
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          buildInputs = [
+            pkgs.openssl
+            pkgs.cyrus_sasl
+            pkgs.curlFull
+          ];
+          nativeBuildInputs = [
+            pkgs.cmake
+            pkgs.pkg-config
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
+        };
+
+        cargoDetails = pkgs.lib.importTOML ./Cargo.toml;
+        inherit (cargoDetails.package) name version;
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        rust-prog = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            meta.mainProgram = name; # This is default in cargo
+          }
+        );
+        dockerTag =
+          if lib.hasAttr "rev" self then "${lib.toString self.revCount}-${self.shortRev}" else "gitDirty";
+        tag = "${version}-${dockerTag}";
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rust-analyzer
+            cargo-watch
+            clippy
+            rustfmt
+
+            kubernetes-helm
+          ];
+          inputsFrom = [ rust-prog ];
+        };
+        formatter = inputs.treefmt-nix.lib.mkWrapper pkgs {
+          programs.nixfmt.enable = true;
+          programs.rustfmt.enable = true;
+        };
+        packages.default = rust-prog;
+        packages.docker = pkgs.dockerTools.buildImage {
+          inherit name tag;
+          config = {
+            Expose = "8000";
+            Workdir = "/app";
+
+            Entrypoint = [ (lib.getExe rust-prog) ];
+            Cmd = [
+              "--config"
+              "/app/config.toml"
+            ];
+          };
+        };
+      }
+    );
+}
